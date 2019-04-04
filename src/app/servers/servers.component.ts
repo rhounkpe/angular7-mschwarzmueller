d269895aaf91c7f7ff8e54514e294f537b4add80
Rufin Hounkpe <rhounkpe@gmail.com>
2019-04-05 00:31:00 +0200

    Components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,7 +4,10 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
